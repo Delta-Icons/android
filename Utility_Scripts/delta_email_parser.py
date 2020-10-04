6114800af7704843a0b1825470aea23020d7ce99
Leif Niemczik <leif.niem@gmail.com>
2020-10-04 17:35:26 +0200

    merging prs

--- Utility_Scripts/delta_email_parser.py
+++ Utility_Scripts/delta_email_parser.py
@@ -13,7 +13,6 @@ from email import policy
 import glob
 import re
 from sys import argv
-from warnings import warn
 
 path = argv[1]
 if not path.endswith('/'):
@@ -21,22 +20,57 @@ if not path.endswith('/'):
 
 filelist = glob.glob(path + '*.eml')
 
-apps = []
-query = re.compile(r'(?P<Name>[\w\d\@\?\/\(\)]+)\s(?P<ComponentInfo>[\w\.\/]+)\shttps://play\.google\.com/store/apps/details\?id=(?P<PackageName>[\w\.]+)', re.M)
+apps = {}
+addresses = []
+query = re.compile(r'(?P<Name>[\w\d\@\?\/\(\)\!]+)\s?(?P<ComponentInfo>[\w\.\/\d]+)\shttps://play\.google\.com/store/apps/details\?id=(?P<PackageName>[\w\.]+)', re.M)
+eMailQuery = re.compile(r'<(.+)>$')
+
+def removeTraitors(traitorAddress, element):
+	if traitorAddress in element['senders']:
+		element['count'] = element['count'] - 1
+		element['senders'] = [x for x in element['senders'] if x is not traitorAddress]
+	return element
+
 
 for mail in filelist:
 	with open(mail, 'rb') as msg:
 		msg = BytesParser(policy=policy.default).parse(msg)
-		text = msg.get_body(preferencelist=('plain')).get_content()
+		parsed= msg.get_body(preferencelist=('plain'))
+		if parsed is None:
+			continue
+		text = parsed.get_content()
 
+		sender = re.search(eMailQuery, msg['From'])
+		if sender is None:
+			continue
+		if sender.groups()[0] in addresses:
+			print('XXXXXX ---- We have a traitor: ', sender.groups()[0])
+			for key, value in apps.items():
+				value = removeTraitors(sender.groups()[0], value)
+			continue
+		else:
+			addresses.append(sender.groups()[0])
 		info = re.search(query, text)
 
 		if info is None:
-			warn("Couldn't handle the following message:\n\n")
-			warn(text)
+			for key, value in apps.items():
+				if key in text:
+					apps[key]['count'] = apps[key]['count'] + 1
+					apps[key]['senders'].append(sender.groups()[0])
+					continue
+			print('\n/// The following message could not be handled:\n',text,'\n')
 		else:
-			apps.append(info.groupdict())
+			tempDict = info.groupdict()
+			if tempDict['ComponentInfo'] in apps:
+				apps[tempDict['ComponentInfo']]['count'] = apps[tempDict['ComponentInfo']]['count'] + 1
+				apps[tempDict['ComponentInfo']]['senders'].append(sender.groups()[0])
+			else:
+				tempDict['count'] = 1
+				tempDict['senders'] = [sender.groups()[0]]
+				apps[tempDict['ComponentInfo']] = tempDict
 
+
+apps = sorted(apps.values(), key=lambda item: item['count'], reverse=True)
 
 updateable = []
 new_apps = []
@@ -44,9 +78,9 @@ new_apps = []
 with open(argv[2]) as appfilter:
 	appfilter = appfilter.read()
 	for app in apps:
-		if appfilter.find(app['PackageName']) is -1 and ''.join(new_apps).find(app['PackageName']) is -1:
-			new_apps.append(app['Name'] + '\n' + app['ComponentInfo'] + '\nhttps://play.google.com/store/apps/details?id=' + app['PackageName'] + '\n\n')
-		elif appfilter.find(app['PackageName']) is not -1 and ''.join(updateable).find(app['PackageName']) is -1:
+		if appfilter.find(app['PackageName']) == -1 and ''.join(new_apps).find(app['PackageName']) == -1:
+			new_apps.append(app['Name'] + '\n' + app['ComponentInfo'] + '\nhttps://play.google.com/store/apps/details?id=' + app['PackageName'] + '\nRequested ' + str(app['count']) + ' times\n\n')
+		elif appfilter.find(app['PackageName']) != -1 and ''.join(updateable).find(app['PackageName']) == -1:
 			updateable.append(app['Name'] + '\n' + app['ComponentInfo'] + '\n\n')
 
 
