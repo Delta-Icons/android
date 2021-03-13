1dbe2135b891c580c1fbe26073f9fe3190b2722a
wokija <jphilippe.gravel@gmail.com>
2021-03-12 21:48:27 -0500

    Added icons + bash script for updates

---
+++ Utility_Scripts/update_requests.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Credit: Marc Bernstein
+# https://stackoverflow.com/questions/2495459/formatting-the-date-in-unix-to-include-suffix-on-day-st-nd-rd-and-th
+DaySuffix() {
+    if [ "x`date +%-d | cut -c2`x" = "xx" ]
+    then
+        DayNum=`date +%-d`
+    else
+        DayNum=`date +%-d | cut -c2`
+    fi
+
+    CheckSpecialCase=`date +%-d`
+    case $DayNum in
+    0 )
+      echo "th" ;;
+    1 )
+      if [ "$CheckSpecialCase" = "11" ]
+      then
+        echo "th"
+      else
+        echo "st"
+      fi ;;
+    2 )
+      if [ "$CheckSpecialCase" = "12" ]
+      then
+        echo "th"
+      else
+        echo "nd"
+      fi ;;
+    3 )
+      if [ "$CheckSpecialCase" = "13" ]
+      then
+        echo "th"
+      else
+        echo "rd"
+      fi ;;
+    [4-9] )
+      echo "th" ;;
+    * )
+      return 1 ;;
+    esac
+}
+
+
+dat_date=$(LANG=en_us_88591; date "+%B %-d`DaySuffix` %Y")
+co_count=$(< $"icon_requests.txt" wc -l)
+co_count="$(( (co_count - 2) / 5))"
+sed -i "2s/.*/$co_count Requests Pending (Updated $dat_date)/" icon_requests.txt
