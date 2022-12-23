e96789e546ab1cf452c7a18261e20ffde9fe7472
creepertron95 <53346722+creepertron95@users.noreply.github.com>
2022-12-23 13:52:54 -0600

    Split alternative icons into a new category.

--- app/src/main/java/website/leifs/delta/applications/CandyBar.java
+++ app/src/main/java/website/leifs/delta/applications/CandyBar.java
@@ -35,10 +35,10 @@ public class CandyBar extends CandyBarApplication {
         configuration.setShadowEnabled(false);
         configuration.setDashboardThemingEnabled(false);
         configuration.setCategoryForTabAllIcons(new String[] {
-                "Google", "Folders", "Calendar", "#", "A", "B", "C", "D", "E", "F", "G", "H", "I",
+                "Google", "Folders", "Calendar", "Alts", "#", "A", "B", "C", "D", "E", "F", "G", "H", "I",
                 "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
         });
-        
+
         return configuration;
     }
 }
