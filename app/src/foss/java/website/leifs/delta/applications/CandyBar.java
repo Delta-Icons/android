83ffc738f27c983e2379fa3c3b811c825160cdb8
flameshikari <flameshikari@users.noreply.github.com>
2024-01-06 03:39:06 +0000

    New beta release: v1.8.8-beta0

--- app/src/foss/java/website/leifs/delta/applications/CandyBar.java
+++ app/src/foss/java/website/leifs/delta/applications/CandyBar.java
@@ -19,7 +19,7 @@ public class CandyBar extends CandyBarApplication {
         Configuration configuration = new Configuration();
 
         configuration.setAutomaticIconsCountEnabled(false);
-        configuration.setCustomIconsCount(9766);
+        configuration.setCustomIconsCount(9812);
         configuration.setDashboardThemingEnabled(false);
         configuration.setGenerateAppFilter(true);
         configuration.setGenerateAppMap(false);
