9986edd383b0fbc57679c51576881904464e0c5e
Co-authored-by: GitGitro <GitGitro@users.noreply.github.com>
2024-09-03 14:38:37 +0000

    Release: v1.9.6-beta0

--- app/src/foss/java/website/leifs/delta/applications/CandyBar.java
+++ app/src/foss/java/website/leifs/delta/applications/CandyBar.java
@@ -19,7 +19,7 @@ public class CandyBar extends CandyBarApplication {
         Configuration configuration = new Configuration();
 
         configuration.setAutomaticIconsCountEnabled(false);
-        configuration.setCustomIconsCount(10773);
+        configuration.setCustomIconsCount(10820);
         configuration.setDashboardThemingEnabled(true);
         configuration.setGenerateAppFilter(true);
         configuration.setGenerateAppMap(false);
