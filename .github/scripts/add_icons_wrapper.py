5103d959054222025d26aaf1632b029ba03a2d0e
Evgeny <mail@hexed.pw>
2024-10-28 00:13:16 +0500

    update scripts (#1000)
    * update add_icons.py
    
    * freeze versions of python packages
    
    * minimal implementation of advanced options for entries in icons.yml
    
    * fix indent in icons.yml
    
    * add predicted entries (by requests_parser.py) in icons.yml
    
    * update CONTRIBUTING.md
    
    * remove lucky_patcher from requests.yml

--- .github/scripts/add_icons_wrapper.py
+++ .github/scripts/add_icons_wrapper.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python3
 
+import argparse
+
 from os import system as execute
 from os import name as platform
-from os.path import basename
+from os.path import basename, join
+from shlex import quote
 
-from natsort import natsorted as sorted
-from yaml import safe_load as yaml
-
-import requests_parser
+from requests_parser import read, write
 from resolve_paths import paths
 
 
 work_dir = paths['scripts']
 delta_dir = paths['root']
-target_file = paths['icons']
-target_script = work_dir + '/add_icons.py'
+
+add_icons = join(work_dir, 'add_icons.py')
+
+icons_file = paths['icons']
 requests_file = paths['requests']
 
-null = '>NUL 2>NUL' if platform == 'nt' else '>/dev/null 2>&1'
 
-content = '''\
+header = '''\
 #
 # new_icon_1:
 #   - com.example/com.example.MainActivity
@@ -30,63 +31,97 @@ content = '''\
 #
 # new_icon_2_alt_1: {}
 #
+# new_icon_3:
+#   category: Google
+#   compinfos:
+#     - com.google.app/com.google.app.MainActivity
 '''
 
-requests = requests_parser.read(requests_file)
 
-with open(target_file) as file:
-    file_parsed = yaml(file)
-    if file_parsed is not None:
-        try:
-            icons = sorted([(k,v) for k,v in file_parsed.items()])[::-1]
+parser = argparse.ArgumentParser()
+parser.add_argument('-D', '--dry-run', action='store_true', help='do things without changing any files')
+options = parser.parse_args()
 
-            errors = []
-            for icon in icons:
-                drawable = icon[0]
-                compinfos = ' '.join(icon[1])
-                command = f'python {target_script} -P {delta_dir} -aidI -n {drawable}'
-                if compinfos: command += f' -c {compinfos}'
-                else:
-                    if drawable.startswith('google_'): command += f' -C Google'
-                    else: command += f' -C Alts'
-                status = execute(f'{command} -D {null}')
-                if status != 0:
-                    print(f'> {drawable}:')
-                    execute(f'{command} -D')
-                    print(command)
-                    print()
-                    errors.append(drawable)
 
-            if errors:
-                errors.sort()
-                print(f'Issues with the next icons: {", ".join(errors)}')
-                exit(1)
+icons_yml = read(icons_file) 
 
-            for icon in icons:
-                drawable = icon[0]
-                compinfos = ' '.join(icon[1])
-                command = f'python {target_script} -P {delta_dir} -aidI -n {drawable}'
+if icons_yml is None:
+    print(f'warn: looks like {basename(icons_file)} is empty')
+    exit(1)
 
-                if compinfos:
-                    command += f' -c {compinfos}'
-                    if drawable.startswith('google_'): command += f' -C Google'
-                    for compinfo in compinfos:
-                        if compinfo in requests:
-                            requests.pop(compinfo)
-                else:
-                    if '_alt_' in drawable:
-                        command += f' -C Alts'
-                print(f'> {drawable}')
-                execute(command)
-                if not icons[-1][0] == drawable: print()
+requests = read(requests_file)
 
-            with open(target_file, 'w', newline='') as file:
-                file.write(content)
+if icons_yml is None:
+    print(f'warn: looks like {basename(icons_file)} is empty')
+    exit(1)
 
-            requests_parser.write(requests_file, requests)
 
-        except Exception as error:
-            print(error)
-            exit(1)
+errors = []
+
+icons = [(key, value) for key, value in icons_yml.items()]
+
+
+command_base = f'python {add_icons} -P {delta_dir} -aidI'
+
+if options.dry_run: command_base += ' -D'
+
+
+for icon in icons:
+    
+    name = icon[0]
+    data = icon[1]
+
+    category = None
+    compinfos = None
+
+
+    if isinstance(data, dict):
+        if len(data) == 0: 
+            print()
+        else:
+            category = data.get('category', None)
+            compinfos = data.get('compinfos', None)
+
+    elif isinstance(data, list):
+        compinfos = icon[1]
     else:
-        print(f'warn: looks like {basename(target_file)} is empty')
+        print('isinstance error')
+        exit(2)
+        
+
+    command = f'{command_base} -n {name}'
+
+    if compinfos:
+        command += ' -c ' + ' '.join(compinfos)
+        for compinfo in compinfos:
+            if compinfo in requests:
+                requests.pop(compinfo)
+    else:
+        if '_alt_' in icon:
+            category = 'Alts'
+
+    if category: command += f' -C {quote(category)}'
+
+    print(f'[icon] {name}')
+    status = execute(f'{command}')
+    print()
+
+
+    if status != 0:
+        errors.append(name)
+    
+    if status == 0:
+        icons_yml.pop(name)
+
+
+if errors:
+    errors.sort()
+    print(f'Issues with the next icons: {", ".join(errors)}')
+    exit(1)
+
+if options.dry_run: exit(0)
+
+with open(icons_file, 'w', newline='') as file:
+    file.write(header)
+
+write(requests_file, requests)
